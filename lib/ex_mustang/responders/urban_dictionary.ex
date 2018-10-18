11ba32f6c160a9dff86b4d2f7c38a24e2ada0420
Denver Abrey <denver@thoughtexpress.com>
2018-10-18 12:36:03 +0200

    Add urbandictionary word of the day responder, resolves #77

---
+++ lib/ex_mustang/responders/urban_dictionary.ex
@@ -0,0 +1,32 @@
+defmodule ExMustang.Responders.UrbanDictionary do
+  @moduledoc """
+  Grabs word of the day from urban dictionary
+  """
+  use Hedwig.Responder
+  import ExMustang.Utils, only: [useragent: 0]
+
+  @feed_url "http://feeds.urbandictionary.com/UrbanWordOfTheDay"
+
+  @usage """
+  Gets word of the day from urbandictionary
+  """
+
+  hear ~r/^urbandictionary$/i, msg do
+    reply(msg, get_ub_wotd())
+  end
+
+  def get_ub_wotd() do
+    case HTTPoison.get(@feed_url, [useragent()]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        case Fiet.parse(body) do
+          {:ok, %Fiet.Feed{items: items}} ->
+            "Word of the day is `#{hd(items).title}`\n#{hd(items).link}"
+          _ ->
+            "Urban Dictionary not available right now"
+        end
+      _ ->
+        "Urban Dictionary not available right now"
+    end
+  end
+
+end
